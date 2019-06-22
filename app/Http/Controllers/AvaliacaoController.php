7ec1748095863d450a0b763db7b8bb434ea77644
BrunoCamarda <bruno_camarda@hotmail.com>
2019-06-21 21:50:08 -0300

    testes

--- app/Http/Controllers/AvaliacaoController.php
+++ app/Http/Controllers/AvaliacaoController.php
@@ -34,7 +34,9 @@ class AvaliacaoController extends Controller
                 $questao[] = $q->id_questao;
             }
             $questoes = Questao::whereIn('id', $questao)->get();
-            return view('avaliacao.show')->with('questoes', $questao);
+            dd($questoes);
+            return $questoes;
+           // return view('avaliacao.show')->with('questoes', $questao);
     //    }else{ 
     //        echo"<script type='text/javascript'>alert('Você não tem permissão para acessar essa página!');
     //        location.href ='/';
